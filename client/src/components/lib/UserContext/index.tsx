3c22549d9ec991c2f2de732ee557490caad59a3c
TanX-009 <tanmaymuley009@gmail.com>
2025-03-17 08:57:52 +0530

    fix(frontend: socket): update to add path from env

--- client/src/components/lib/UserContext/index.tsx
+++ client/src/components/lib/UserContext/index.tsx
@@ -5,7 +5,9 @@ import io, { Socket } from "socket.io-client";
 import generateRoomCode from "@/lib/generateRoomCode";
 import generateUsername from "@/lib/generateUsername";
 
-const socket: Socket = io(process.env.NEXT_PUBLIC_SERVER_API_URL); // Adjust the URL as needed
+const socket: Socket = io(process.env.NEXT_PUBLIC_SERVER_API_URL, {
+  path: process.env.NEXT_PUBLIC_SERVER_API_PATH,
+}); // Adjust the URL as needed
 
 interface TProps {
   readonly children: React.ReactNode;
